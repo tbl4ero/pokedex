7daff4a6a4c72e5d058b7116433f1c3ab5e72d17
Erik <cartman21000@gmail.com>
2021-07-20 07:11:40 +1000

    Updates

---
+++ precache-manifest.3b5dcec7911c1de5dd00b7857a2e6e8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "473972f290aae1c03360330836fcf9cf",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "2a2ecd455e26ec935d3f",
+    "url": "/pokedex/static/css/main.e7583bce.chunk.css"
+  },
+  {
+    "revision": "bb91a7f182e29d9dce2d",
+    "url": "/pokedex/static/js/2.67ebd5f6.chunk.js"
+  },
+  {
+    "revision": "a1f4cc4ba7b23972ccc7e6cdc81d0eef",
+    "url": "/pokedex/static/js/2.67ebd5f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a2ecd455e26ec935d3f",
+    "url": "/pokedex/static/js/main.c3eba82d.chunk.js"
+  },
+  {
+    "revision": "a1c3345951e6945697c3",
+    "url": "/pokedex/static/js/runtime-main.6f9bb1fe.js"
+  }
+]);
